8ec5e7980f31488cf4f1b8de856d17f2a1fa90db
1tccsinan <csipes.nandi77@gmail.com>
2025-09-12 12:31:26 +0200

    szorzotabla

--- ciklus.py
+++ ciklus.py
@@ -21,3 +21,34 @@ for elem in lista:
 
 print("Páros számok darabszáma:", db)
 
+#írass ki egy sorba 5 *-ot
+
+print("*"*5)
+
+for i in range(5):
+    print("*", end="")
+print()
+
+#rajzolj priamist
+#       *
+#      ***
+#     *****
+#    *******
+#   *********
+#  ***********
+
+n = 5
+space_db = n - 1
+csillag_db = 1
+for i in range(n):
+    print(" " * space_db, end=" ")
+    print("*" * csillag_db) 
+    space_db -= 1
+    csillag_db += 2
+
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i*j:4}" , end=" ")
+    print()
+
