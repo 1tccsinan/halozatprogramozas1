f6c8dbeafb5f4d43dbd09a3d62334d76c6c049c7
1tccsinan <csipes.nandi77@gmail.com>
2025-09-12 12:58:05 +0200

    szamok osszege

--- ciklus2.py
+++ ciklus2.py
@@ -1,1 +1,15 @@
 #Kérjünk be számokat és adjuk meg az összegüket
+
+# while bennmaradasi feltétel:
+#     ciklus mag
+
+
+
+osszeg = 0
+be_szam=None
+while be_szam !=0 :
+    be_szam=int(input("Kérem a számot: "))
+    osszeg += be_szam
+
+print(f"A számok összege {osszeg}")
+
