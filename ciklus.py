757a063dbb8f96cc06d817eac2974c69794cc391
1tccsinan <csipes.nandi77@gmail.com>
2025-09-12 11:55:28 +0200

    parosdb

--- ciklus.py
+++ ciklus.py
@@ -1,1 +1,23 @@
 #ciklusok
+
+lista = [1, 2, 3, 4, 5]
+
+
+def osszegez(lista):
+    osszeg = 0
+    for elem in lista :
+        osszeg += elem
+    return osszeg
+
+
+
+print(f"A lista elemeinek osszege: {osszegez(lista)}")
+
+
+db = 0
+for elem in lista:
+    if elem % 2 == 0:
+        db += 1
+
+print("Páros számok darabszáma:", db)
+
